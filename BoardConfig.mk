805978ce5612ba4d928393d0792ff7e44f9cf148
kvsnr113 <kvsnrprojkt113@gmail.com>
2026-04-26 20:49:53 +0700

    munch: change kernel defconfig path for my kernel source
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 TARGET_SCREEN_DENSITY := 440
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/munch.config
+TARGET_KERNEL_CONFIG := vendor/munch_defconfig
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := munch,munch_global,munch_in
