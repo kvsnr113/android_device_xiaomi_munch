b6750dba377926e918a848a08d3c3037ba9bf536
kvsnr113 <kvsnrprojkt113@gmail.com>
2026-04-27 22:39:51 +0700

    munch: inherit my signing private keys
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- device.mk
+++ device.mk
@@ -49,5 +49,8 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Inherit Private Keys
+-include vendor/lineage-priv/keys/keys.mk
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/munch/munch-vendor.mk)
