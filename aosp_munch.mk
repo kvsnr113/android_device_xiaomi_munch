5e1ec48ab0b905a3d81feb3a7fd8f6f171a173ae
kvsnr113 <kvsnrprojkt113@gmail.com>
2026-05-05 10:15:53 +0700

    munch: initial setup for androidone experience
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

---
+++ aosp_munch.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2021-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit from munch device
+$(call inherit-product, device/xiaomi/munch/device.mk)
+
+PRODUCT_NAME := aosp_munch
+PRODUCT_DEVICE := munch
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := 22021211RC
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="munch-user 13 RKQ1.211001.001 V816.0.9.0.ULMMIXM release-keys" \
+    BuildFingerprint=Redmi/munch/munch:13/RKQ1.211001.001/V816.0.9.0.ULMMIXM:user/release-keys
+
+TARGET_SUPPORT_MINIMAL_GAPPS := true
+TARGET_SUPPORT_PIXEL_LAUNCHER := true
+TARGET_HAS_GEMINI_BOOTANIMATION	:= false
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORT_LIVE_WALLPAPERS := false
+GMS_VOICE_MODEL_INCLUDED := false
