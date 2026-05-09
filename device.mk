72086216dd98d07b162f34135792272ec9060113
kvsnr113 <kvsnrprojkt113@gmail.com>
2026-05-10 00:45:19 +0700

    Revert "munch: ArrowOS bringup"
    This reverts commit 0f78599435ec1ebece7cb1b9a198811191f71300.

--- device.mk
+++ device.mk
@@ -13,7 +13,7 @@ $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-lineage
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
@@ -24,6 +24,8 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_SHIPPING_API_LEVEL := 30
+
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 
 # Audio configs
 PRODUCT_COPY_FILES += \
